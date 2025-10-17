d7f7739df2bd1aa9f21aa74462ca327971cf4f25
AnchalAgrahari <anchalagrahari258@gmail.com>
2025-10-17 23:22:02 +0530

    inserted the info in the table

--- employee/insert_info.sql
+++ employee/insert_info.sql
@@ -1,13 +1,2 @@
--- INSERT INTO BankDatabase (name, lname, email, hire_data)
--- VALUES ('Shubham', 'Agrahari','shubhamagrahri03@gmail.com','2015-09-25');
--- SELECT * FROM BankDatabase
-
--- update BankDatabase
--- 	set hire_data = '2015-09-25'
--- 	where emp_id = '2';
-
-UPDATE BankDatabase 
-	SET DEPT = 'Accounts'
-	WHERE name = 'Shubham';
-
-SELECT * FROM BankDatabase
+INSERT INTO BankDatabase (emp_id, name, lname, email, dept, salary, hire_data)
+values 
