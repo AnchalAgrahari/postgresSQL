e1e4567e6f5b7cb6a3c98d374f3408ab497416fb
AnchalAgrahari <anchalagrahari258@gmail.com>
2025-10-23 22:56:15 +0530

    Aggerate function

---
+++ employee/aggeratefunction.sql
@@ -0,0 +1,13 @@
+select COUNT(name) from bankdatabase;  
+
+
+select sum(salary) from bankdatabase;
+
+
+select avg(salary) from bankdatabase;
+
+
+select min(salary) from bankdatabase;
+
+
+select max(salary) from bankdatabase;
