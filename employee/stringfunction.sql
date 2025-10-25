2fc5136b676da9c3811e820befc6233e7f89cc4a
AnchalAgrahari <anchalagrahari258@gmail.com>
2025-10-25 22:13:18 +0530

    Data modification function in SQL

---
+++ employee/stringfunction.sql
@@ -0,0 +1,14 @@
+select emp_id, concat_ws (' ',name,lname) as Fullname, dept from bankdatabase;
+
+
+-- hello as example
+select substr('hello ', 1,6);
+
+
+select replace('asdrf' ,'asd', 'abc');
+--- work as 'word', 'replaced', 'replaced to'
+
+select reverse ('hello') --print backward
+
+select length ('hello')
+
