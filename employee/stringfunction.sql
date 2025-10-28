da4bbebb2fcdc6f16ddd15acf6c6da3ad4bbba53
AnchalAgrahari <anchalagrahari258@gmail.com>
2025-10-28 23:20:21 +0530

    sring frunction for the modification of the data

--- employee/stringfunction.sql
+++ employee/stringfunction.sql
@@ -1,14 +1,28 @@
-select emp_id, concat_ws (' ',name,lname) as Fullname, dept from bankdatabase;
+--select emp_id, concat_ws (' ',name,lname) as Fullname, dept from bankdatabase;
 
 
 -- hello as example
-select substr('hello ', 1,6);
+select substr('hello ',1,4);
+
+--replace( str, from_str, to_str)
+select replace ('hello ','hello', 'hi buddy');
+
+--perint the value in reverse
+select reverse('hello');
+
+-- length of the vale
+select length ('asfreliyjrtjknh');
+
+-- UPPER and lower Case
+select upper('hello sir');
+select lower ('HELLO');
+
+--left right tirm
+select left('adaskfjsd',4);
+select right ('asafjndsekt',6);
+select trim ('     safbsdf      ');
 
 
-select replace('asdrf' ,'asd', 'abc');
---- work as 'word', 'replaced', 'replaced to'
-
-select reverse ('hello') --print backward
-
-select length ('hello')
+-- position
+select position ('om' in ' thomas');
 
