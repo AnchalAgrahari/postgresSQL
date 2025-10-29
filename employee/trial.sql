7dfafd34242d92870643595d5831eba6dc7242f2
AnchalAgrahari <anchalagrahari258@gmail.com>
2025-10-29 23:30:30 +0530

    Updated files

--- employee/trial.sql
+++ employee/trial.sql
@@ -1,12 +1,9 @@
 select * from bankdatabase;
 
-select
-concat(
-left(dept::text,2),
-' ',
-right(emp_id::text,1), 
-' ', 
-name
-) as value
-from bankdatabase;
+select concat(left(dept::text,1),right(emp_id::text,1),' ',name ) as value from bankdatabase;
 
+
+
+
+
+select substr(dept,1,1) from bankdatabase;
